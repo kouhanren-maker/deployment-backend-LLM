8502d48ce1bbb2e1ff3d6f04ebd0ea232d9e4878
Gavin <kouhanren@gmail.com>
2025-11-01 14:03:42 +1100

    Initial commit for AI Agent deployment

--- shopping/app/admin.py
+++
@@ -1,9 +0,0 @@
-# app/admin.py
-# from django.http import JsonResponse
-# from .models import User
-# from .auth_decorators import admin_required
-
-# @admin_required
-# def list_all_users(request):
-#     users = User.objects.all().values("id", "email", "role", "create_time")
-#     return JsonResponse({"users": list(users)}, safe=False)
